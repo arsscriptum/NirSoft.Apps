3a7590f499455af91c549df4894144f67c4c3658
arsscriptum <guillaumeplante.qc@gmail.com>
2023-10-06 18:51:20 -0400

    auto

--- scripts/Combine.ps1
+++ scripts/Combine.ps1
@@ -3,7 +3,7 @@ $RootPath = (Resolve-Path "$PSScriptRoot\..").Path
 $SplitFilesPath   = Join-Path $RootPath "split_files"
 $ScriptPath = Join-Path $RootPath "Scripts"
 $ReCombinedDataPath = Join-Path $RootPath "recombined_files"
-$ReCombinedFilePath = Join-Path $ReCombinedDataPath "File.pdf"
+$ReCombinedFilePath = Join-Path $ReCombinedDataPath "nirsoft.zip"
 $FileUtils = Join-Path $ScriptPath "FileUtils.ps1"
 
 . "$FileUtils"
